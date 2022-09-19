1d5cc152d502436a22632b2a91a1bad471997b6e
tianyusun7 <tianyusun@ucsb.edu>
2022-09-19 15:41:51 -0700

    update OptionsDao.java

--- src/main/java/com/flag/travelplanner/dao/OptionsDao.java
+++ src/main/java/com/flag/travelplanner/dao/OptionsDao.java
@@ -1,10 +1,12 @@
 package com.flag.travelplanner.Dao;
 
 import com.flag.travelplanner.entity.Option;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Repository
@@ -12,16 +14,51 @@ public class OptionsDao {
     @Autowired
     private SessionFactory sessionFactory;
     public List<Option> getOneDayOptions() {
-
-        return null;
+        List<Option> result = new ArrayList<>();
+        List<Option> optionList = getOptionList();
+        for (Option option : optionList) {
+            if (option.getDays() == 1) {
+                result.add(option);
+            }
+        }
+        return result;
     }
     public List<Option> getThreeDayOptions() {
-        return null;
+        List<Option> result = new ArrayList<>();
+        List<Option> optionList = getOptionList();
+        for (Option option : optionList) {
+            if (option.getDays() == 3) {
+                result.add(option);
+            }
+        }
+        return result;
     }
-
     public List<Option> getSevenDayOptions() {
-        return null;
+        List<Option> result = new ArrayList<>();
+        List<Option> optionList = getOptionList();
+        for (Option option : optionList) {
+            if (option.getDays() == 7) {
+                result.add(option);
+            }
+        }
+        return result;
     }
 
-    public List<Option> getOptionList() {return null;}
+    public List<Option> getOptionList() {
+        Session session = null;
+        try {
+            session = sessionFactory.openSession();
+            session.beginTransaction();
+            //return...
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            if (session != null) {
+                session.getTransaction().rollback();
+            }
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
 }
